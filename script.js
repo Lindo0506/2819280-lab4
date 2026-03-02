9b22da4c3b48466bdaf72c6300ba67aea539543e
Lindo0506 <2819280@students.wits.ac.za>
2026-03-02 16:51:34 +0200

    Final version before submission

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ async function searchCountry(countryName) { try { if (!countryName.trim()) { thr
     countryInfo.innerHTML = '';
     bordersContainer.innerHTML = '';
 
-    const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}`);
+    const response = await fetch(`https://restcountries.com/v3.1/name/${countryName}?fullText=true`);
     if (!response.ok) {
         throw new Error('Country not found. Please try again.');
     }
